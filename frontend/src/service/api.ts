ec3265dd680c5d0d0797f638a2103fef99b1b187
backendcoders <teambackendcoders@gmail.com>
2026-04-24 17:49:24 +0530

    adding the remote url

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: 'http://localhost:5000',
-	// baseURL: 'https://api.backendcodersindia.com',
+	// baseURL: 'http://localhost:5000',
+	baseURL: 'https://api.backendcodersindia.com',
 	timeout: 100000,
 	headers: {
 		'Content-Type': 'application/json',
